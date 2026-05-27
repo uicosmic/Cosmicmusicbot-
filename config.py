5135ee4b19161c961226f26f331b5f0276e63630
uicosmic <aryanray8249@gmail.com>
2026-05-27 12:11:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+SESSION_NAME = os.getenv("SESSION_NAME", "")
